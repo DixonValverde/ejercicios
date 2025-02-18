8a3d48e0993e02bc096c7325ed435e74406d91ac
Boris Dixon <164239667+DixonValverde@users.noreply.github.com>
2025-02-17 21:46:43 -0500

    17/2/2025

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNum2 = new System.Windows.Forms.TextBox();
+            this.tbNum1 = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.sumar = new System.Windows.Forms.CheckBox();
+            this.restar = new System.Windows.Forms.CheckBox();
+            this.multiplicar = new System.Windows.Forms.CheckBox();
+            this.dividir = new System.Windows.Forms.CheckBox();
+            this.modulo = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Lime;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(195, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(195, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero2";
+            // 
+            // tbNum2
+            // 
+            this.tbNum2.Location = new System.Drawing.Point(187, 162);
+            this.tbNum2.Name = "tbNum2";
+            this.tbNum2.Size = new System.Drawing.Size(100, 20);
+            this.tbNum2.TabIndex = 2;
+            this.tbNum2.Text = "5";
+            // 
+            // tbNum1
+            // 
+            this.tbNum1.Location = new System.Drawing.Point(187, 89);
+            this.tbNum1.Name = "tbNum1";
+            this.tbNum1.Size = new System.Drawing.Size(100, 20);
+            this.tbNum1.TabIndex = 3;
+            this.tbNum1.Text = "4";
+            this.tbNum1.TextChanged += new System.EventHandler(this.tbNum1_TextChanged);
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.BackColor = System.Drawing.Color.White;
+            this.btncalcular.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btncalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcular.Location = new System.Drawing.Point(187, 279);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(75, 23);
+            this.btncalcular.TabIndex = 4;
+            this.btncalcular.Text = "calcular";
+            this.btncalcular.UseVisualStyleBackColor = false;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MV Boli", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(195, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Operaciones";
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.Location = new System.Drawing.Point(190, 330);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(72, 16);
+            this.result.TabIndex = 6;
+            this.result.Text = "resultado";
+            // 
+            // sumar
+            // 
+            this.sumar.AutoSize = true;
+            this.sumar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sumar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.sumar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sumar.Location = new System.Drawing.Point(157, 202);
+            this.sumar.Name = "sumar";
+            this.sumar.Size = new System.Drawing.Size(40, 21);
+            this.sumar.TabIndex = 7;
+            this.sumar.Text = "+";
+            this.sumar.UseVisualStyleBackColor = true;
+            // 
+            // restar
+            // 
+            this.restar.AutoSize = true;
+            this.restar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.restar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.restar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restar.Location = new System.Drawing.Point(209, 202);
+            this.restar.Name = "restar";
+            this.restar.Size = new System.Drawing.Size(37, 21);
+            this.restar.TabIndex = 8;
+            this.restar.Text = "-";
+            this.restar.UseVisualStyleBackColor = true;
+            // 
+            // multiplicar
+            // 
+            this.multiplicar.AutoSize = true;
+            this.multiplicar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.multiplicar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.multiplicar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multiplicar.Location = new System.Drawing.Point(257, 202);
+            this.multiplicar.Name = "multiplicar";
+            this.multiplicar.Size = new System.Drawing.Size(38, 21);
+            this.multiplicar.TabIndex = 9;
+            this.multiplicar.Text = "*";
+            this.multiplicar.UseVisualStyleBackColor = true;
+            // 
+            // dividir
+            // 
+            this.dividir.AutoSize = true;
+            this.dividir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dividir.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.dividir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dividir.Location = new System.Drawing.Point(158, 240);
+            this.dividir.Name = "dividir";
+            this.dividir.Size = new System.Drawing.Size(37, 21);
+            this.dividir.TabIndex = 10;
+            this.dividir.Text = "/";
+            this.dividir.UseVisualStyleBackColor = true;
+            // 
+            // modulo
+            // 
+            this.modulo.AutoSize = true;
+            this.modulo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.modulo.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.modulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.modulo.Location = new System.Drawing.Point(214, 240);
+            this.modulo.Name = "modulo";
+            this.modulo.Size = new System.Drawing.Size(45, 21);
+            this.modulo.TabIndex = 11;
+            this.modulo.Text = "%";
+            this.modulo.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(540, 397);
+            this.Controls.Add(this.modulo);
+            this.Controls.Add(this.dividir);
+            this.Controls.Add(this.multiplicar);
+            this.Controls.Add(this.restar);
+            this.Controls.Add(this.sumar);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.tbNum1);
+            this.Controls.Add(this.tbNum2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbNum2;
+        private System.Windows.Forms.TextBox tbNum1;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.CheckBox sumar;
+        private System.Windows.Forms.CheckBox restar;
+        private System.Windows.Forms.CheckBox multiplicar;
+        private System.Windows.Forms.CheckBox dividir;
+        private System.Windows.Forms.CheckBox modulo;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+
+    {
+        Matematica mat;
+
+
+        public Form1( )
+
+        {
+
+            InitializeComponent();
+        }
+
+        
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            double num1, num2;
+
+            if (!double.TryParse(tbNum1.Text, out num1))
+            {
+                MessageBox.Show("Por favor, ingrese un numero valido en el primer campo :D");
+                return; 
+            }
+            if (!double.TryParse(tbNum2.Text, out num2))
+            {
+                MessageBox.Show("Por favor, ingrese un numero valido en el segundo campo :D");
+                return; 
+            }
+            mat = new Matematica(num1, num2);
+            calcularoperacion();
+
+
+
+
+        }
+
+        private void calcularoperacion()
+        {
+            double resultado = 0;
+            if (sumar.Checked)
+            {
+                mat.sumar();
+                MessageBox.Show("La suma es: " + mat.sumar());
+                result.Text = mat.sumar().ToString();
+            }
+            if (restar.Checked)
+            {
+                mat.restar();
+                MessageBox.Show("La resta es: " + mat.restar());
+                result.Text = mat.restar().ToString();
+            }
+            if (multiplicar.Checked)
+            {
+                mat.multiplicar();
+                MessageBox.Show("La multiplicación es: " + mat.multiplicar());
+                result.Text = mat.multiplicar().ToString();
+            }
+            if (dividir.Checked)
+            {
+                if (mat.getNum2() == 0)
+                {
+                    MessageBox.Show("No se puede dividir por cero");
+                    result.Text = "Error";
+                    return;
+                }
+                resultado = mat.dividir();
+                MessageBox.Show("La división es: " + resultado);
+                result.Text = resultado.ToString();
+            }
+            if (modulo.Checked)
+            {
+                if (mat.getNum2() == 0)
+                {
+                    MessageBox.Show("No se puede calcular el módulo cuando el divisor es cero");
+                    result.Text = "Error";
+                    return;
+                }
+                resultado = mat.modulo();
+                MessageBox.Show("El modulo es: " + resultado);
+                result.Text = resultado.ToString();
+            }
+
+
+        }
+
+        private void tbNum1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
